dc53dc18d69ba1114168c02c0205b0c91fb03bbf
Tony Gustafsson <mail@tonyg.se>
2019-01-23 22:20:52 +0100

    Run prettier

--- assets/js/musicControl.js
+++ assets/js/musicControl.js
@@ -1,165 +1,173 @@
 function changeSound(e, audio) {
-	"use strict";
-	
-	var thisSound = e.target.options[e.target.selectedIndex].value,
-		soundsForm = document.getElementById('sounds-form'),
-		soundPath = soundsForm.getAttribute('data-sounds-path') + thisSound,
-		urlId = 'sound' + audio.id;
+    'use strict';
 
-	if (thisSound === "0") {
-		audio.src = "";
-		return;
-	}
-	
-	audio.type = 'audio/ogg';
-	audio.src = soundPath;
-	audio.load();
+    var thisSound = e.target.options[e.target.selectedIndex].value,
+        soundsForm = document.getElementById('sounds-form'),
+        soundPath = soundsForm.getAttribute('data-sounds-path') + thisSound,
+        urlId = 'sound' + audio.id;
 
-	queryHelper.changeParameter(urlId, 'src', thisSound);
+    if (thisSound === '0') {
+        audio.src = '';
+        return;
+    }
+
+    audio.type = 'audio/ogg';
+    audio.src = soundPath;
+    audio.load();
+
+    queryHelper.changeParameter(urlId, 'src', thisSound);
 }
 
 function changeVolume(e, audio, volumeIcon) {
-	"use strict";
-	audio.volume = e.target.value / 100;
+    'use strict';
+    audio.volume = e.target.value / 100;
 
-	switch(true) {
-		case e.target.value > 80:
-			volumeIcon.className = 'icon-volume-high';
-			break;
-		case e.target.value > 40:
-			volumeIcon.className = 'icon-volume-medium';
-			break;
-		case e.target.value > 0:
-			volumeIcon.className = 'icon-volume-low';
-			break;
-		default:
-			volumeIcon.className = 'icon-volume-mute';
-	}
+    switch (true) {
+        case e.target.value > 80:
+            volumeIcon.className = 'icon-volume-high';
+            break;
+        case e.target.value > 40:
+            volumeIcon.className = 'icon-volume-medium';
+            break;
+        case e.target.value > 0:
+            volumeIcon.className = 'icon-volume-low';
+            break;
+        default:
+            volumeIcon.className = 'icon-volume-mute';
+    }
 
-	var urlId = 'sound' + audio.id;
-	queryHelper.changeParameter(urlId, 'vol', e.target.value);
+    var urlId = 'sound' + audio.id;
+    queryHelper.changeParameter(urlId, 'vol', e.target.value);
 }
 
 function changeState(e, audio, stateIcon) {
-	"use strict";
-	var newState;
+    'use strict';
+    var newState;
 
-	if (!audio.paused) {
-		audio.pause();
-		stateIcon.className = 'icon-play';
-		newState = 'off';
-	}
-	else {
-		audio.play();
-		stateIcon.className = 'icon-pause';
-		newState = 'on';
-	}
+    if (!audio.paused) {
+        audio.pause();
+        stateIcon.className = 'icon-play';
+        newState = 'off';
+    } else {
+        audio.play();
+        stateIcon.className = 'icon-pause';
+        newState = 'on';
+    }
 
-	var urlId = 'sound' + audio.id;
-	queryHelper.changeParameter(urlId, 'state', newState);
+    var urlId = 'sound' + audio.id;
+    queryHelper.changeParameter(urlId, 'state', newState);
 }
 
 function createNewSoundControl(id) {
-	"use strict";
+    'use strict';
 
-	var	soundsForm = document.getElementById('sounds-form'),
-		numberOfSounds = parseInt(soundsForm.getElementsByTagName('select').length, 10),
-		fieldset = document.createElement('fieldset'),
-		legend = document.createElement('legend'),
-		id = (id !== undefined) ? id : numberOfSounds + 1,
-		legendText = document.createTextNode(' Sound ' + id),
-		legendIcon = document.createElement('span'),
-		select = document.createElement('select'),
-		stateControl = document.createElement('button'),
-		stateIcon = document.createElement('span'),
-		volumeControl = document.createElement('input'),
-		volumeIcon = document.createElement('span'),
-		audio = new Audio(),
-		x;
+    var soundsForm = document.getElementById('sounds-form'),
+        numberOfSounds = parseInt(soundsForm.getElementsByTagName('select').length, 10),
+        fieldset = document.createElement('fieldset'),
+        legend = document.createElement('legend'),
+        id = id !== undefined ? id : numberOfSounds + 1,
+        legendText = document.createTextNode(' Sound ' + id),
+        legendIcon = document.createElement('span'),
+        select = document.createElement('select'),
+        stateControl = document.createElement('button'),
+        stateIcon = document.createElement('span'),
+        volumeControl = document.createElement('input'),
+        volumeIcon = document.createElement('span'),
+        audio = new Audio(),
+        x;
 
-	fieldset.id = 'sound' + id + '-container';
-	legendIcon.className = 'icon-music';
-	legend.appendChild(legendIcon);
-	legend.appendChild(legendText);
+    fieldset.id = 'sound' + id + '-container';
+    legendIcon.className = 'icon-music';
+    legend.appendChild(legendIcon);
+    legend.appendChild(legendText);
 
-	select.id = 'sound' + id + '-sound';
+    select.id = 'sound' + id + '-sound';
 
-	select.addEventListener('change', function(e) {
-		changeSound(e, audio);
+    select.addEventListener('change', function(e) {
+        changeSound(e, audio);
 
-		if (soundsForm.lastChild.getElementsByTagName('select')[0] === this) {
-			//Create a new sound form if this is the last one
-			createNewSoundControl();
-		}
-	});
+        if (soundsForm.lastChild.getElementsByTagName('select')[0] === this) {
+            //Create a new sound form if this is the last one
+            createNewSoundControl();
+        }
+    });
 
-	for (x in soundFiles) {
-    	if (!soundFiles.hasOwnProperty(x)) continue;
+    for (x in soundFiles) {
+        if (!soundFiles.hasOwnProperty(x)) continue;
 
-		var option = document.createElement('option');
-		option.setAttribute('value', soundFiles[x]);
-		option.text = soundFiles[x];
-		select.appendChild(option);
-	}
+        var option = document.createElement('option');
+        option.setAttribute('value', soundFiles[x]);
+        option.text = soundFiles[x];
+        select.appendChild(option);
+    }
 
-	volumeIcon.className = 'icon-volume-medium';
-	volumeControl.setAttribute('type', 'range');
-	volumeControl.setAttribute('min', 0);
-	volumeControl.setAttribute('max', 100);
-	volumeControl.setAttribute('value', 80);
-	volumeControl.addEventListener('change', function(e) { changeVolume(e, audio, volumeIcon); });
+    volumeIcon.className = 'icon-volume-medium';
+    volumeControl.setAttribute('type', 'range');
+    volumeControl.setAttribute('min', 0);
+    volumeControl.setAttribute('max', 100);
+    volumeControl.setAttribute('value', 80);
+    volumeControl.addEventListener('change', function(e) {
+        changeVolume(e, audio, volumeIcon);
+    });
 
-	stateIcon.className = 'icon-pause';
-	stateControl.setAttribute('type', 'button');
-	stateControl.appendChild(stateIcon);
-	stateControl.addEventListener('click', function(e) { changeState(e, audio, stateIcon); });
+    stateIcon.className = 'icon-pause';
+    stateControl.setAttribute('type', 'button');
+    stateControl.appendChild(stateIcon);
+    stateControl.addEventListener('click', function(e) {
+        changeState(e, audio, stateIcon);
+    });
 
-	audio.id = id;
+    audio.id = id;
 
-	audio.addEventListener("canplay", function() {
-		audio.play(); 
-	});
+    audio.addEventListener('canplay', function() {
+        audio.play();
+    });
 
-	audio.addEventListener("ended", function() {
-		audio.play(); 
-	});
+    audio.addEventListener('ended', function() {
+        audio.play();
+    });
 
-	fieldset.appendChild(legend);
-	fieldset.appendChild(select);
-	fieldset.appendChild(volumeControl);
-	fieldset.appendChild(volumeIcon);
-	fieldset.appendChild(stateControl);
-	fieldset.appendChild(audio);
+    fieldset.appendChild(legend);
+    fieldset.appendChild(select);
+    fieldset.appendChild(volumeControl);
+    fieldset.appendChild(volumeIcon);
+    fieldset.appendChild(stateControl);
+    fieldset.appendChild(audio);
 
-	soundsForm.appendChild(fieldset);
+    soundsForm.appendChild(fieldset);
 
-	return fieldset;
+    return fieldset;
 }
 
 (function start() {
-	var queries = queryHelper.getQueries(),
-		select, option, soundId, fieldset,
-		i, j, k;
+    var queries = queryHelper.getQueries(),
+        select,
+        option,
+        soundId,
+        fieldset,
+        i,
+        j,
+        k;
 
-	queries = {};
+    queries = {};
 
-	for (i in queries) {
-    	if (!queries.hasOwnProperty(i)) continue;
+    for (i in queries) {
+        if (!queries.hasOwnProperty(i)) continue;
 
-		if (queries[i].src !== undefined) {
-			soundId = i.replace("sound", "");
-			fieldset = createNewSoundControl(soundId);
-			select = document.getElementById(i + '-sound');
+        if (queries[i].src !== undefined) {
+            soundId = i.replace('sound', '');
+            fieldset = createNewSoundControl(soundId);
+            select = document.getElementById(i + '-sound');
 
-			for (j = 0, k = select.options.length; j < k; j = j + 1) {
-				option = select.options[j];
+            for (j = 0, k = select.options.length; j < k; j = j + 1) {
+                option = select.options[j];
 
-				if (option.value === queries[i].src) {
-					select.selectedIndex = j;
-				}
-			}
-		}
-	}
+                if (option.value === queries[i].src) {
+                    select.selectedIndex = j;
+                }
+            }
+        }
+    }
 
-	createNewSoundControl();
+    createNewSoundControl();
 })();
--- assets/js/queryHelper.js
+++ assets/js/queryHelper.js
@@ -1,86 +1,93 @@
 var queryHelper = {
-	getQueries: function() {
-		"use strict";
+    getQueries: function() {
+        'use strict';
 
-		var queryString = window.location.search.replace("?", ""),
-			queryObj = {};
+        var queryString = window.location.search.replace('?', ''),
+            queryObj = {};
 
-		if (queryString === "") {
-			console.log('No query yet... return empty.');
-			return queryObj;
-		}
+        if (queryString === '') {
+            console.log('No query yet... return empty.');
+            return queryObj;
+        }
 
-		var	urlSettings = queryString.split('&'),
-			urlIdKeyVals, soundSettings, soundSetting,
-			urlId, urlKey, urlVal,
-			i, l, j, k;
+        var urlSettings = queryString.split('&'),
+            urlIdKeyVals,
+            soundSettings,
+            soundSetting,
+            urlId,
+            urlKey,
+            urlVal,
+            i,
+            l,
+            j,
+            k;
 
-		for (i = 0, l = urlSettings.length; i < l; i = i + 1) {
-			soundSettings = urlSettings[i].split('=');
-			urlId = soundSettings[0];
-			soundSetting = soundSettings[1].split(';');
+        for (i = 0, l = urlSettings.length; i < l; i = i + 1) {
+            soundSettings = urlSettings[i].split('=');
+            urlId = soundSettings[0];
+            soundSetting = soundSettings[1].split(';');
 
-			for (j = 0, k = soundSetting.length; j < k; j = j + 1) {
-				urlIdKeyVals = soundSetting[j].split(':');
-				urlKey = urlIdKeyVals[0];
-				urlVal = urlIdKeyVals[1];
+            for (j = 0, k = soundSetting.length; j < k; j = j + 1) {
+                urlIdKeyVals = soundSetting[j].split(':');
+                urlKey = urlIdKeyVals[0];
+                urlVal = urlIdKeyVals[1];
 
-				if (queryObj[urlId] === undefined) {
-					queryObj[urlId] = {};
-				}
+                if (queryObj[urlId] === undefined) {
+                    queryObj[urlId] = {};
+                }
 
-				queryObj[urlId][urlKey] = urlVal;
-			}
-		}
+                queryObj[urlId][urlKey] = urlVal;
+            }
+        }
 
-		return queryObj;
-	},
-	getUrl: function(queries) {
-		"use strict";
+        return queryObj;
+    },
+    getUrl: function(queries) {
+        'use strict';
 
-		if (queries === undefined) {
-			queries = this.getQueries();
-		}
+        if (queries === undefined) {
+            queries = this.getQueries();
+        }
 
-		var lastQueryKey = Object.keys(queries)[Object.keys(queries).length - 1],
-			lastQuerySegmentKey,
-			url = '?',
-			i, l;
+        var lastQueryKey = Object.keys(queries)[Object.keys(queries).length - 1],
+            lastQuerySegmentKey,
+            url = '?',
+            i,
+            l;
 
-		for (i in queries) {
-			lastQuerySegmentKey = Object.keys(queries[i])[Object.keys(queries[i]).length - 1];
-			url = url + i + '=';
+        for (i in queries) {
+            lastQuerySegmentKey = Object.keys(queries[i])[Object.keys(queries[i]).length - 1];
+            url = url + i + '=';
 
-			for (l in queries[i]) {
-				url = url + l + ':' + queries[i][l];
+            for (l in queries[i]) {
+                url = url + l + ':' + queries[i][l];
 
-				if (lastQuerySegmentKey !== l) {
-					url = url + ';';
-				}
-			}
+                if (lastQuerySegmentKey !== l) {
+                    url = url + ';';
+                }
+            }
 
-			if (lastQueryKey !== i) {
-				url = url + '&';
-			}
-		}
+            if (lastQueryKey !== i) {
+                url = url + '&';
+            }
+        }
 
-		return url;
-	},
-	changeParameter: function(urlId, urlKey, urlVal) {
-		"use strict";
+        return url;
+    },
+    changeParameter: function(urlId, urlKey, urlVal) {
+        'use strict';
 
-		var queries = this.getQueries(),
-			newUrl;
+        var queries = this.getQueries(),
+            newUrl;
 
-		if (queries[urlId] === undefined) {
-			queries[urlId] = {};
-		}
+        if (queries[urlId] === undefined) {
+            queries[urlId] = {};
+        }
 
-		queries[urlId][urlKey] = urlVal;
+        queries[urlId][urlKey] = urlVal;
 
+        newUrl = queryHelper.getUrl(queries);
 
-		newUrl = queryHelper.getUrl(queries);
-
-		history.pushState(null, null, newUrl);
-	}
+        history.pushState(null, null, newUrl);
+    }
 };
